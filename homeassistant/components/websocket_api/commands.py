bc06282dde1b4304670c03d9faf85c32917612ef
Paulus Schoutsen <balloob@gmail.com>
2019-03-05 15:10:50 -0800

    Add WS subscription command for MQTT

--- homeassistant/components/websocket_api/commands.py
+++ homeassistant/components/websocket_api/commands.py
@@ -76,15 +76,6 @@ SCHEMA_PING = messages.BASE_COMMAND_MESSAGE_SCHEMA.extend({
 })
 
 
-def event_message(iden, event):
-    """Return an event message."""
-    return {
-        'id': iden,
-        'type': TYPE_EVENT,
-        'event': event.as_dict(),
-    }
-
-
 def pong_message(iden):
     """Return a pong message."""
     return {
@@ -107,7 +98,9 @@ def handle_subscribe_events(hass, connection, msg):
         if event.event_type == EVENT_TIME_CHANGED:
             return
 
-        connection.send_message(event_message(msg['id'], event))
+        connection.send_message(messages.event_message(
+            msg['id'], event.as_dict()
+        ))
 
     connection.event_listeners[msg['id']] = hass.bus.async_listen(
         msg['event_type'], forward_events)
